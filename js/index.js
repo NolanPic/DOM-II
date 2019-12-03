3dea8242de90ce80f70f3cf29e1f56ebd066cd89
Nolan Picini <nolanpic@gmail.com>
2019-12-03 13:28:36 -0800

    Added first two transitions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,73 @@
 // Your code goes here
+
+// array of ipsum
+
+const WORDS = [
+    'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+    'adipiscing', 'elit', 'curabitur', 'vel', 'hendrerit', 'libero',
+    'eleifend', 'blandit', 'nunc', 'ornare', 'odio', 'ut',
+    'orci', 'gravida', 'imperdiet', 'nullam', 'purus', 'lacinia',
+    'a', 'pretium', 'quis', 'congue', 'praesent', 'sagittis', 
+    'laoreet', 'auctor', 'mauris', 'non', 'velit', 'eros',
+    'dictum', 'proin', 'accumsan', 'sapien', 'nec', 'massa',
+    'volutpat', 'venenatis', 'sed', 'eu', 'molestie', 'lacus',
+    'quisque', 'porttitor', 'ligula', 'dui', 'mollis', 'tempus',
+    'at', 'magna', 'vestibulum', 'turpis', 'ac', 'diam',
+    'tincidunt', 'id', 'condimentum', 'enim', 'sodales', 'in',
+    'hac', 'habitasse', 'platea', 'dictumst', 'aenean', 'neque',
+    'fusce', 'augue', 'leo', 'eget', 'semper', 'mattis', 
+    'tortor', 'scelerisque', 'nulla', 'interdum', 'tellus', 'malesuada',
+    'rhoncus', 'porta', 'sem', 'aliquet', 'et', 'nam',
+    'suspendisse', 'potenti', 'vivamus', 'luctus', 'fringilla', 'erat',
+    'donec', 'justo', 'vehicula', 'ultricies', 'varius', 'ante',
+    'primis', 'faucibus', 'ultrices', 'posuere', 'cubilia', 'curae',
+    'etiam', 'cursus', 'aliquam', 'quam', 'dapibus', 'nisl',
+    'feugiat', 'egestas', 'class', 'aptent', 'taciti', 'sociosqu',
+    'ad', 'litora', 'torquent', 'per', 'conubia', 'nostra',
+    'inceptos', 'himenaeos', 'phasellus', 'nibh', 'pulvinar', 'vitae',
+    'urna', 'iaculis', 'lobortis', 'nisi', 'viverra', 'arcu',
+    'morbi', 'pellentesque', 'metus', 'commodo', 'ut', 'facilisis',
+    'felis', 'tristique', 'ullamcorper', 'placerat', 'aenean', 'convallis',
+    'sollicitudin', 'integer', 'rutrum', 'duis', 'est', 'etiam',
+    'bibendum', 'donec', 'pharetra', 'vulputate', 'maecenas', 'mi',
+    'fermentum', 'consequat', 'suscipit', 'aliquam', 'habitant', 'senectus',
+    'netus', 'fames', 'quisque', 'euismod', 'curabitur', 'lectus',
+    'elementum', 'tempor', 'risus', 'cras'
+];
+
+const state = {
+    scale: '1'
+};
+
+// grab all elements and set their transition
+const allElements = document.querySelectorAll('*');
+allElements.forEach(el => {
+    el.style.transition = '0.5s';
+});
+
+// grab all paragraphs
+const pElements = document.querySelectorAll('p');
+
+// EVENT ONE -- mousemove
+pElements.forEach(el => {
+    el.addEventListener('mousemove', () => {
+
+        // this should be fun...
+        pElements.forEach(p => {
+            // get random word
+            const randomWord = WORDS[Math.floor(Math.random()*WORDS.length)];
+            p.textContent = p.textContent + ' ' + randomWord;
+        });
+    });
+});
+
+// EVENT TWO -- scroll
+window.addEventListener('scroll', () => {
+    const newScale = state.scale === '1' ? '1.01' : '1';
+
+    allElements.forEach(el => {
+        el.style.transform = `scale(${newScale})`;
+    });
+
+    state.scale = newScale;
+});
